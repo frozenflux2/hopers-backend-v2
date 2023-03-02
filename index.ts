aebc4c373e7f19cddc76399626ee4dcbcfa00065
lumi518 <aurora518star.com@gmail.com>
2023-03-02 09:42:16 +0800

    modified mongoose setting

--- index.ts
+++ index.ts
@@ -6,7 +6,8 @@ import app from './src/app';
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const mongooseURI = process.env.MONGOOSE_URI;
+// const mongooseURI = process.env.MONGOOSE_URI;
+const mongooseURI = process.env.MONGODB_URI;
 
 mongoose
     .connect(mongooseURI)
