a1062d2921bc224dd9334cc3a227e978a6d8a908
lumi518 <aurora518star.com@gmail.com>
2023-03-02 22:58:15 +0800

    added debugging route and codes

--- src/controller/debug.controller.ts
+++ src/controller/debug.controller.ts
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
+import { MAX_FETCH_ITEMS } from '../constants';
+import Collection from '../models/collection.model';
+import { MarketplaceInfo } from '../types';
+import { getDataFromDB, runQuery } from '../utils';
+
+dotenv.config();
+
+const MarketplaceContract = process.env.MARKETPLACE_CONTRACT;
 
 const formatMemoryUsage = (data) =>
     `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
@@ -29,4 +38,43 @@ export const getMemoryUsage = async (
     } catch (e) {
         res.status(400).send({ message: e.message });
     }
+};
+
+export const test = async (
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+) => {
+    const fetchCollectionBidsByAddress = async (address: string) => {
+        let offers: any[] = [];
+        const fetchCollectionBids = async (startAfter?: any) => {
+            const fetchedBidsResult = await runQuery(MarketplaceContract, {
+                collection_bid_by_collection: {
+                    collection: address,
+                    start_after: startAfter,
+                    limit: MAX_FETCH_ITEMS,
+                },
+            });
+            const fetchedBids = fetchedBidsResult?.bids || [];
+            offers = offers.concat(fetchedBids);
+            if (fetchedBids.length === MAX_FETCH_ITEMS) {
+                await fetchCollectionBids(
+                    fetchedBids[MAX_FETCH_ITEMS - 1].bidder,
+                );
+            }
+        };
+        await fetchCollectionBids();
+        return offers;
+    };
+    const collections: any = await getDataFromDB(Collection);
+    const queries = collections.map((collection: MarketplaceInfo) =>
+        fetchCollectionBidsByAddress(collection.nftContract),
+    );
+    Promise.all(queries)
+        .then((queryResults) => {
+            res.status(200).send({ collections, queryResults });
+        })
+        .catch((err) => {
+            res.status(400).send({ collections, message: err.message });
+        });
 };
