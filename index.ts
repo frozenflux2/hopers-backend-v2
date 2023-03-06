5e36568c24bcf27b29f833eb37b6d652f6fc769d
lumi518 <aurora518star.com@gmail.com>
2023-03-06 09:17:04 +0800

    made update logic functions promise

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './src/app';
-import mainLogic from './src/logic';
+// import mainLogic from './src/logic';
 // const port = config.port;
 dotenv.config();
 
@@ -13,7 +13,7 @@ mongoose
     .connect(mongooseURI)
     .then(() => {
         console.log('Connected to MongoDB');
-        mainLogic();
+        // mainLogic();
         app.listen(port, () => {
             console.log(`Listening to port ${port}`);
             // mainLogic();
--- src/utils/async.ts
+++ src/utils/async.ts
@@ -1,15 +1,27 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-export const getDataFromDB = (DB: mongoose.Model<any>, query?: any): Promise<mongoose.Document[]> => new Promise((resolve, reject) => {
-    DB.find(query || {}, (err: any, docs: mongoose.Document[]) => {
-        if (err) reject(err)
-        resolve(docs)
-    })
-})
+export const makePromiseFunction = (func: () => Promise<any>) =>
+    new Promise((resolve, reject) => {
+        func()
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
 
-export const saveData = (doc: mongoose.Document): Promise<mongoose.Document> => new Promise((resolve, reject) => {
-    doc.save((updateErr, updated) => {
-        if (updateErr) reject(updateErr)
-        return resolve(updated)
-    })
-})
+export const getDataFromDB = (
+    DB: mongoose.Model<any>,
+    query?: any,
+): Promise<mongoose.Document[]> =>
+    new Promise((resolve, reject) => {
+        DB.find(query || {}, (err: any, docs: mongoose.Document[]) => {
+            if (err) reject(err);
+            resolve(docs);
+        });
+    });
+
+export const saveData = (doc: mongoose.Document): Promise<mongoose.Document> =>
+    new Promise((resolve, reject) => {
+        doc.save((updateErr, updated) => {
+            if (updateErr) reject(updateErr);
+            return resolve(updated);
+        });
+    });
